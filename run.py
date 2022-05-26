58d49f7a1977aafc0a44935fdbc2e9454d280ab2
mark john <mark.john@editmatters.co.uk>
2022-05-26 02:33:05 +0000

    Add create ships function to battleship class

--- run.py
+++ run.py
@@ -24,3 +24,14 @@ class Battleship:
     def __init__(self, board):
         self.board = board
 
+    def create_ships(self):
+        for i in range(5):
+            self.x_row, self.y_column = random.randint(0, 7), 
+            random.randint(0, 7)
+            while self.board[self.x_row][self.y_column] == 'x':
+                self.x_row, self.y_column = random.randint(0, 7), 
+                random.randint(0, 7)
+            self.board[self.x_row][self.y_column] = 'x'
+
+        return self.board
+
